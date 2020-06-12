aea2281b519220de8d51edb343e5edf716a3c5cf
Viktor Zatorskyi <victor.zatorski@gmail.com>
2020-06-12 16:40:18 +0300

    Frame time added to stats

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -8,11 +8,13 @@ public class FPSCounter : MonoBehaviour
     //-----------------------------------------------------------------
     [SerializeField]
     private Text m_FPSText;
+    [SerializeField]
+    private Text m_FrameTimeText;
     //-----------------------------------------------------------------
     private void Update()
     {
-        int fps = (int) (1.0f / Time.deltaTime);
-        m_FPSText.text = $"FPS: { fps }";
+        m_FPSText.text = $"FPS: {( 1.0f / Time.deltaTime ),5:N0}";
+        m_FrameTimeText.text = $"FrameTime(ms): {(Time.deltaTime * 1000.0f),5:N1}";
     }
     //-----------------------------------------------------------------
 }
